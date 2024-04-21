4230db5034345d4f5bdbe20e09fe8f5607a2fa95
Mahima Gupta <131277652+Mahimatestgithub@users.noreply.github.com>
2024-04-21 15:38:14 +0530

    Time: 7 ms (98.73%), Space: 21.7 MB (37.63%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,9 +1,10 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
+        //shift all the non zeros element to left
         int nonzero=0;
         for(int j=0;j<nums.size();j++){
-            if(nums[j]!=0){
+            if(nums[j]!=0){    //j value ko non zero indez m daal do
             swap(nums[j],nums[nonzero]);
             nonzero++;
             }
