f17dcf6b18a09c7a6f5d140f28003700c354093f
Mahima Gupta <131277652+Mahimatestgithub@users.noreply.github.com>
2024-04-21 15:59:41 +0530

    Time: 23 ms (36.71%), Space: 28 MB (14.12%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -4,7 +4,7 @@ public:
         vector<int>temp(nums.size());
         for(int i=0;i<nums.size();i++)
         {
-         temp[(i+k)%nums.size()] = nums[i];   
+         temp[(i+k)%nums.size()] = nums[i];    //taking each number from its original position and putting it into a new position, shifted                                                to the right by k places, wrapping around if needed.
         }
         //copy temp into num vector
         
