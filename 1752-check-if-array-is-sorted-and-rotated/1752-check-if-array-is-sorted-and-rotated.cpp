9dfea57f8cea7c8346875b6d3a35b501727360c7
Mahima Gupta <131277652+Mahimatestgithub@users.noreply.github.com>
2024-04-21 16:51:34 +0530

    Time: 0 ms (100.00%), Space: 10.1 MB (88.32%) - LeetHub

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
@@ -4,11 +4,11 @@ public:
      int count=0;
         int n = nums.size();
         for(int i=1;i<n;i++){
-            if(nums[i-1] > nums[i]){
+            if(nums[i-1] > nums[i]){     
                 count++;
             }
         }
-        if(nums[n-1]>nums[0])
+        if(nums[n-1]>nums[0])   //last element is greater than 0th index
             count++;
          return count<=1;
     }
